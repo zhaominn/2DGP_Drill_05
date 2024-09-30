5ebd82a0d1c9ff96a3447d165085bb885ce4e7fd
zhaominn <somin207@naver.com>
2024-09-30 15:59:01 +0900

    기본적인 입력(X, ESC)을 받는다

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,8 +7,17 @@ ground = load_image('TUK_GROUND.png')
 
 
 def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
     pass
-    
+
 
 frame = 0
 running = True
