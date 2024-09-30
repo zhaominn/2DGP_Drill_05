1fdcb5f944515c9b2333304d0f7c0cc9725fbc5d
zhaominn <somin207@naver.com>
2024-09-30 15:57:17 +0900

    키 입력을 받는 함수 생성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,6 +5,11 @@ open_canvas()
 character = load_image('BODY_skeleton.png')
 ground = load_image('TUK_GROUND.png')
 
+
+def handle_events():
+    pass
+    
+
 frame = 0
 running = True
 
@@ -13,6 +18,7 @@ while running:
     ground.draw(400,300)
     character.clip_draw(frame*64,64,64,64,400,300)
     update_canvas()
+    handle_events()
     frame = (frame+1) % 9
     delay(0.05)
 
