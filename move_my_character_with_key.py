ce83867d7e1864b33952d1ff2905bbaecd6c8966
zhaominn <somin207@naver.com>
2024-09-30 16:01:19 +0900

    방향키 입력을 받아 캐릭터를 움직이게 한다

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,28 +7,51 @@ ground = load_image('TUK_GROUND.png')
 
 
 def handle_events():
-    global running
+    global running, dirX, dirY
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
+            if event.key == SDLK_RIGHT:
+                dirX += 1
+            elif event.key == SDLK_LEFT:
+                dirX -= 1
+            elif event.key == SDLK_UP:
+                dirY += 1
+            elif event.key == SDLK_DOWN:
+                dirY -= 1
+            elif event.key == SDLK_ESCAPE:
                 running = False
-    pass
+        elif event.type==SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dirX-=1
+            elif event.key == SDLK_LEFT:
+                dirX+=1
+            elif event.key == SDLK_UP:
+                dirY-=1
+            elif event.key == SDLK_DOWN:
+                dirY+=1
 
-
-frame = 0
 running = True
+x = 800 // 2
+y= 600 // 2
+frame = 0
+dirX = 0
+dirY = 0
+
 
 while running:
     clear_canvas()
-    ground.draw(400,300)
-    character.clip_draw(frame*64,64,64,64,400,300)
+    ground.draw(400, 300)
+    character.clip_draw(frame * 64, 64, 64, 64, x, y)
     update_canvas()
     handle_events()
-    frame = (frame+1) % 9
+    frame = (frame + 1) % 9
+    x += dirX * 5
+    y += dirY * 5
     delay(0.05)
 
 close_canvas()
