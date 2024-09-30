a8128f4e90ed53b4dd9618e64ee6ff971299b052
zhaominn <somin207@naver.com>
2024-09-30 16:11:05 +0900

    방향키에 따라 다른 스트라이트 이미지 생성을 위한 함수 생성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -35,6 +35,10 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 dirY+=1
 
+def draw_character():
+    character.clip_draw(frame * 64, 64, 64, 64, x, y)
+    pass
+
 running = True
 x = 800 // 2
 y= 600 // 2
@@ -46,7 +50,7 @@ dirY = 0
 while running:
     clear_canvas()
     ground.clip_draw(0,0, 1280, 1024, 400, 300,800,600)
-    character.clip_draw(frame * 64, 64, 64, 64, x, y)
+    draw_character()
     update_canvas()
     handle_events()
     frame = (frame + 1) % 9
